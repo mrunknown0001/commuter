2d1ef5ca17994ba76f670da052948332bb0287eb
Michael Adam <madamt0001@gmail.com>
2018-07-09 00:43:09 +0800

    Added terms and conditions

--- resources/views/includes/terms-and-condition.blade.php
+++ resources/views/includes/terms-and-condition.blade.php
@@ -7,7 +7,29 @@
                 <h4 class="modal-title">Terms and Condition</h4>
             </div>
             <div class="modal-body">
-                <p>Content</p>
+                <p><strong>By using the Software or the Application, you agree that:</strong></p>
+                <ul>
+                <li>You will only use the Service for lawful purposes;</li>
+                <li>You will only use the Service for the purpose for which it is intended to be used;</li>
+                <li>You will not use the Application for sending or storing any unlawful material or for fraudulent purposes;</li>
+                <li>You will not use the Application and/or the Software to cause nuisance, annoyance, inconvenience or make fake bookings;</li>
+                <li>You will not use the Service, the Application and/or the Software for purposes other than obtaining the Service;</li>
+                <li>You shall not contact the third party transportation provider for purposes other than the Service;</li>
+                <li>You will not impair the proper operation of the network;</li>
+                <li>You shall not intentionally or unintentionally cause or attempt to cause damage to the third party transportation provider;</li>
+                <li>You will not try to harm the Service, the Application and/or the Software in any way whatsoever;</li>
+                <li>You shall not employ any means defraud the School or enrich yourself, through any means, whether fraudulent or otherwise, through any event, promotion or campaign launched by the Company to encourage new subscription or usage of the Service by new or existing passengers;</li>
+                <li>You shall not impair or circumvent the proper operation of the network which the Service operates on;</li>
+                <li>You agree that the Service is provided on a reasonable effort basis; and </li>
+                <li>You agree that your use of the Service will be subject to the School’s Privacy Policy as may be amended from time to time.</li>
+                <li>You will not copy, or distribute the Software or other content without written permission from the School;</li>
+                <li>You will only use the Software and/or the Application for your own use and will not resell it to a third party;</li>
+                <li>You will keep secure and confidential your account password or any identification we provide you which allows access to the Service;</li>
+                <li>You will provide the School with proof of identity as it is may reasonably request or require;</li>
+                <li>You acknowledge and agree that only one (1) account can be registered on one device;</li>
+                <li>You agree to provide accurate, current and complete information as required for the Service and undertake the responsibility to maintain and update your information in a timely manner to keep it accurate, current and complete at all times during the term of this Agreement. You agree that the School will rely on your information as accurate, current and complete. You acknowledge that if your information is untrue, inaccurate, not current or incomplete in any respect, the School has the right but not the obligation to terminate this Agreement and your use of the Service at any time with or without notice;<br>
+                You agree to assume full responsibility and liability for all loss or damage suffered by yourself, the third party service provider, the School or any third party as a result of any breach of the Terms of Use.</li>
+                </ul>
             </div>
             <div class="modal-footer">
                 <small>Terms and Condition</small>
