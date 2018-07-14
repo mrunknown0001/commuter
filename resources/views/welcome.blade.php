a342139977871d99272c266db747eac0b0f1cc34
Michael Adam <madamt0001@gmail.com>
2018-07-14 23:26:55 +0800

    Added full single background image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,54 +7,27 @@
     <meta name="author" content="">
     <title>Welcome to Commuter Monitoring System</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <style type="text/css">
-      .masthead {
-        min-height: 30rem;
-        position: relative;
-        display: table;
-        width: 100%;
-        height: auto;
-        padding-top: 20rem;
-        padding-bottom: 8rem;
-        background-position: center center;
-        background-repeat: no-repeat;
-        background-size: cover;
-      }
-      .masthead h1 {
-        font-size: 4rem;
-        margin: 0;
-        padding: 0;
-      }
-      @media (min-width: 992px) {
-        .masthead {
-          height: 100vh;
-        }
-        .masthead h1 {
-          font-size: 5.5rem;
-        }
-      }
-      @media (max-width: 600px) {
-        li {
-            height: 30px;
-            line-height: 30px;
-            padding: 20px;
-        }
-      }
-    </style>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/full-bg.css') }}">
   </head>
   <body>
-    <header class="masthead d-flex">
-      <div class="container text-center my-auto">
-        <h1 class="">Commuter Queuing System</h1>
-        <h3 class="mb-5">
-        </h3>
-        <ul class="list-inline">
-          <li><a href="{{ route('login') }}" class="btn btn-primary">Commuter &amp; Driver Login</a></li>
-          <li><a href="{{ route('commuter.registration') }}" class="btn btn-primary">Commuter Registration</a></li>
-          <li><a href="{{ route('driver.registration') }}" class="btn btn-primary">Driver Registration</a></li>
-        </ul>
+
+    <section>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h1>Commuter Monitoring System</h1>
+            <ul class="list-inline">
+              <li><a href="{{ route('login') }}" class="btn btn-primary">Commuter &amp; Driver Login</a></li>
+              <li><a href="{{ route('commuter.registration') }}" class="btn btn-primary">Commuter Registration</a></li>
+              <li><a href="{{ route('driver.registration') }}" class="btn btn-primary">Driver Registration</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div class="overlay"></div>
-    </header>
+    </section>
+
+    <script src="{{ asset('js/jquery.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+
   </body>
 </html>
