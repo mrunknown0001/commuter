950f5481c6f409414a0a9f18a7244ad88baed1a1
Michael Adam <madamt0001@gmail.com>
2018-07-20 18:10:23 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,73 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Welcome to Commuter Monitoring System</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/full-bg.css') }}">
-    <style type="text/css">
-      li a {
-        margin-top: 15px;
-      }
-      h1 {
-        color: #fff;
-        text-shadow: 2px 2px 4px #000000;
-      }
-      .container {
-        margin-top: 250px;
-      }
-    </style>
-  </head>
-  <body>
 
-    <section>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Welcome</title>
+
+  <!-- Google font -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CVarela+Round" rel="stylesheet">
+
+  <!-- Bootstrap -->
+  <link type="text/css" rel="stylesheet" href="{{ asset('css/app.css') }}" />
+
+  <!-- Owl Carousel -->
+  <link type="text/css" rel="stylesheet" href="{{ asset('landing/css/owl.carousel.css') }}" />
+  <link type="text/css" rel="stylesheet" href="{{ asset('landing/css/owl.theme.default.css') }}" />
+
+  <!-- Magnific Popup -->
+  <link type="text/css" rel="stylesheet" href="{{ asset('landing/css/magnific-popup.css') }}" />
+
+  <!-- Custom stlylesheet -->
+  <link type="text/css" rel="stylesheet" href="{{ asset('landing/css/style.css') }}" />
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <style type="text/css">
+    a {
+      margin-top: 5px;
+    }
+  </style>
+</head>
+
+<body>
+  <header id="home">
+    <div class="bg-img" style="background-image: url('/uploads/bg.jpg');">
+      <div class="overlay"></div>
+    </div>
+    
+    <div class="home-wrapper">
       <div class="container">
         <div class="row">
-          <div class="col-md-6 col-md-offset-3">
-            <h1>Commuter Monitoring System</h1>
-            <ul class="list-inline">
-              <li><a href="{{ route('login') }}" class="btn btn-primary">Commuter &amp; Driver Login</a></li>
-              <li><a href="{{ route('commuter.registration') }}" class="btn btn-primary">Commuter Registration</a></li>
-              <li><a href="{{ route('driver.registration') }}" class="btn btn-primary">Driver Registration</a></li>
-            </ul>
+          <div class="col-md-10 col-md-offset-1">
+            <div class="home-content">
+              <h1 class="white-text">{{ env('app_name') }}</h1>
+              <!-- <p class="white-text">App Description</p> -->
+              <a href="{{ route('login') }}" class="btn btn-primary btn-lg">Commuter &amp; Driver Login</a>
+              <a href="{{ route('commuter.registration') }}" class="btn btn-primary btn-lg">Commuter Registration</a>
+              <a href="{{ route('driver.registration') }}" class="btn btn-primary btn-lg">Driver Registration</a>
+            </div>
           </div>
+
         </div>
       </div>
-    </section>
+    </div>
 
-    <script src="{{ asset('js/jquery.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+  </header>
 
-  </body>
+  <script src="{{ asset('js/jquery.js') }}"></script>
+  <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('landing/js/owl.carousel.min.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('landing/js/jquery.magnific-popup.js') }}"></script>
+  <script type="text/javascript" src="{{ asset('landing/js/main.js') }}"></script>
+
+</body>
 </html>
