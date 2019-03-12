72f45a28a2e3d77651e616c2d4a5aab176f62d29
Michael Adam <madamt0001@gmail.com>
2019-03-13 03:47:21 +0800

    Update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -39,7 +39,7 @@
         <!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-green">
+          <div class="small-box bg-red">
             <div class="inner">
               <h3>{{ count($current_rides) }}</h3>
 
@@ -54,7 +54,7 @@
         <!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-green">
+          <div class="small-box bg-blue">
             <div class="inner">
               <h3>{{ count($commuters) }}</h3>
 
@@ -69,7 +69,7 @@
         <!-- ./col -->
         <div class="col-lg-3 col-xs-6">
           <!-- small box -->
-          <div class="small-box bg-green">
+          <div class="small-box bg-orange">
             <div class="inner">
               <h3>{{ count($drivers) }}</h3>
 
